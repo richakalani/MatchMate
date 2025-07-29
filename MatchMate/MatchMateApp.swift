75575c3ec621a3b561e8b7fd64f8b9021e081dab
Richa Kalani <richakalani07@gmail.com>
2025-07-29 18:07:27 +0530

    Initial Commit

---
+++ MatchMate/MatchMateApp.swift
@@ -0,0 +1,20 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by Richa Kalani on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
